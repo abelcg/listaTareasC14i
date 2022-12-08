7c8db2793a4a6b581660712ea2a6c29ed53c6974
abelcg <abelcg86@gmail.com>
2022-12-07 21:39:13 -0300

    se inicio el proyecto lista tareas

---
+++ src/components/Subtitulo.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Subtitulo extends Component {
+    render() {
+        return (
+            <p className="text-center lead">Lista de tareas usando React y localStorage</p>
+        );
+    }
+}
+
+export default Subtitulo;
